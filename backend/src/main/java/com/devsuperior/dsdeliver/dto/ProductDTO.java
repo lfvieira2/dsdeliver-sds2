f8ab5bae4d8f605c5d93edc88af66055b018488e
Luiz Filipe <luiz.filipe.vieira@hotmail.com>
2021-02-07 09:30:48 -0300

    Get /products

--- backend/src/main/java/com/devsuperior/dsdeliver/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/ProductDTO.java
@@ -1,16 +1,14 @@
-package com.devsuperior.dsdeliver.entities;
+package com.devsuperior.dsdeliver.dto;
 
 import java.io.Serializable;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
-@Entity
-@Table(name = "tb_product")
-public class Product implements Serializable {
+import com.devsuperior.dsdeliver.entities.Product;
+
+public class ProductDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -21,16 +19,16 @@ public class Product implements Serializable {
 	private String description;
 	private String imageUri;
 
-	public Product() {
+	public ProductDTO() {
+
 	}
 
-	public Product(Long id, String name, Double price, String description, String imageUri) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.price = price;
-		this.description = description;
-		this.imageUri = imageUri;
+	public ProductDTO(Product product) {
+		this.id = product.getId();
+		this.name = product.getName();
+		this.price = product.getPrice();
+		this.description = product.getDescription();
+		this.imageUri = product.getImageUri();
 	}
 
 	public Long getId() {
@@ -71,31 +69,6 @@ public class Product implements Serializable {
 
 	public void setImageUri(String imageUri) {
 		this.imageUri = imageUri;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Product other = (Product) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
 	}
 
 }
